bff99dbfc11777c8a750841062f55a2813c996b9
bryan <invictagaming239@gmail.com>
2024-07-22 08:14:21 +0100

    first

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -23,9 +24,12 @@ const PotholeSchema = new mongoose.Schema({
 const Pothole = mongoose.model("Pothole", PotholeSchema);
 
 // Middleware
-app.use(cors()); // Add this line to enable CORS
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // POST endpoint to receive pothole data
 app.post("/api/pothole", async (req, res) => {
