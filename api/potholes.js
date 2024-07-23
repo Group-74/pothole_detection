81b0be8bbe7eef101c61da36a23f15b6d78d4c90
bryan <invictagaming239@gmail.com>
2024-07-23 02:49:15 +0100

    second

--- api/potholes.js
+++ api/potholes.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const mongoURI =
   "mongodb+srv://bryanojji4:6DcKLQOfVrZkjYBw@potholesite.6wt2x95.mongodb.net/?retryWrites=true&w=majority&appName=potholesite";
@@ -18,38 +24,40 @@ const PotholeSchema = new mongoose.Schema({
 });
 const Pothole = mongoose.model("Pothole", PotholeSchema);
 
-module.exports = async (req, res) => {
+app.get("/api/potholes", async (req, res) => {
   console.log("Request received:", req.method, req.url);
 
-  if (req.method === "GET") {
-    try {
-      const potholes = await Pothole.find();
-      res.status(200).json(potholes);
-    } catch (err) {
-      console.error("Error retrieving data:", err);
-      res.status(500).json({ message: "Error retrieving data", error: err });
-    }
-  } else if (req.method === "POST") {
-    const { lat, lng } = req.body;
-    try {
-      const existingPothole = await Pothole.findOne({
-        latitude: lat,
-        longitude: lng,
-      });
-      if (existingPothole) {
-        return res.status(200).json({ message: "Duplicate pothole data" });
-      }
-
-      const newPothole = new Pothole({ latitude: lat, longitude: lng });
-      await newPothole.save();
-      res
-        .status(200)
-        .json({ message: "Pothole data received", data: newPothole });
-    } catch (err) {
-      console.error("Error saving data:", err);
-      res.status(400).json({ message: "Error saving data", error: err });
-    }
-  } else {
-    res.status(405).json({ message: "Method Not Allowed" });
+  try {
+    const potholes = await Pothole.find();
+    res.status(200).json(potholes);
+  } catch (err) {
+    console.error("Error retrieving data:", err);
+    res.status(500).json({ message: "Error retrieving data", error: err });
   }
-};
+});
+
+app.post("/api/potholes", async (req, res) => {
+  const { lat, lng } = req.body;
+  try {
+    const existingPothole = await Pothole.findOne({
+      latitude: lat,
+      longitude: lng,
+    });
+    if (existingPothole) {
+      return res.status(200).json({ message: "Duplicate pothole data" });
+    }
+
+    const newPothole = new Pothole({ latitude: lat, longitude: lng });
+    await newPothole.save();
+    res
+      .status(200)
+      .json({ message: "Pothole data received", data: newPothole });
+  } catch (err) {
+    console.error("Error saving data:", err);
+    res.status(400).json({ message: "Error saving data", error: err });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("API server running on port 3001");
+});
